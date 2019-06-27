3ca3ce2eb117af9314c1a1f4999fc5bfeac9047f
songyiao <164110657@qq.com>
2019-06-27 14:05:14 +0800

    1

--- src/main/java/com/song/springboot/SpringBootJdbcApplication.java
+++ src/main/java/com/song/springboot/SpringBootJdbcApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
+//111
 @MapperScan(value = "com.song.springboot.mapper")
 @SpringBootApplication
 @EnableCaching
@@ -12,6 +12,7 @@ public class SpringBootJdbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJdbcApplication.class, args);
+        System.out.println("1111");
     }
 
 }
